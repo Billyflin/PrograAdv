927944fc68e415dfb2645a06ee4140293b8ed583
Billy <billymartinezc@gmail.com>
2022-10-02 02:43:32 -0300

    taller 5

--- martinez/Taller5/tres.php
+++ martinez/Taller5/tres.php
@@ -5,43 +5,31 @@
 </head>
 <body>
 <?php
-//$color = "blue";
-//$row = 10;
-//$col = 15;
-$color=$_GET['color'];
-$row=$_GET['row'];
-$col=$_GET['col'];
-function deg_selector($color){
-if($color=="red"){
-    $color=0;
-}else if($color=="green"){
-    $color=240;
-}else if($color=="blue"){
-    $color=120;
-}else if($color=="yellow"){
-    $color=60;
-}
-return $color;
-}
-function paint($color,$row,$col){
-    $deg=deg_selector($color);
-    $light=100/$col;
-    $sat=100/$row;
-    echo "<table cellspacing='0' cellpadding='0'>";
-    for ($i = 0; $i < $row; $i++) {
-        echo "\n<tr >";
-        for ($j = 0; $j < $col; $j++) {
-            if((100-($light*$j))==100){
-                $c = "hsl(".$deg.",".($sat*$i)."%, ".(95)."%)";
-            }else{
-                $c = "hsl(".$deg.",".($sat*$i)."%, ".(100-($light*$j))."%)";
+
+$color = $_GET['color'];
+$row = $_GET['row'];
+$col = $_GET['col'];
+
+
+function get($color, $row, $col): void
+{
+    $gradiente = 30 / $row;
+    $deg = $color - 12;
+    $light = 100 / $col;
+    echo "<table  cellpadding='0' cellspacing='1px' >";
+    for ($j = 0; $j < $col; $j++) {
+        echo "<tr>";
+        for ($i = 0; $i < $row; $i++) {
+            $l = $light * $j;
+            $hsl = "hsl(" . ($deg + ($gradiente * $i)) . ", 100%, $l%)";
+            if ($l < 95 && $l > 7) {
+                echo "<td  style='background-color: $hsl; width: 50px; height: 50px; '></td>";
             }
-            echo "\n<td style='background-color:$c;height: 40px;width: 40px ' > ". " </td>";
         }
-        echo "\n</tr>";
     }
 }
-paint($color,$row,$col);
+
+get($color, $row, $col);
 ?>
 
 <h1 id="div"></h1>
